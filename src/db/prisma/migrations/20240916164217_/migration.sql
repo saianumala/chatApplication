e453e880c4ac8520dc547434d8783ec5624e9ac4
saianumala <anumalansk@gmail.com>
2024-12-03 12:41:34 +0530

    removed nodumodules and js files

--- src/db/prisma/migrations/20240916164217_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[savedById,mobileNumber]` on the table `Contact` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Contact_savedById_mobileNumber_key" ON "Contact"("savedById", "mobileNumber");
